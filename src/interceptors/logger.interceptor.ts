5d196c9d12f4f4383b6222503aa3878d5226ac26
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-05-30 20:01:15 +0300

    implement DRY coding style on the interceptor

--- src/interceptors/logger.interceptor.ts
+++ src/interceptors/logger.interceptor.ts
@@ -6,8 +6,17 @@ import {
 } from '@nestjs/common'
 import { Observable } from 'rxjs'
 import { tap, finalize } from 'rxjs/operators'
-
 import { Request, Response } from 'express'
+
+const log = (req: Request, res: Response, prevMs: number) => () => {
+   const finish = Date.now()
+   const took = (finish - prevMs).toFixed(3)
+   console.log(
+      `${req.method} ${req.originalUrl} ${res.statusCode} from ${
+         req.headers['x-forwarded-for'] || req.ip
+      } ${took} ms ${req.headers.origin || ''}`
+   )
+}
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -16,25 +25,8 @@ export class LoggingInterceptor implements NestInterceptor {
       const req: Request = context.switchToHttp().getRequest()
       const res: Response = context.switchToHttp().getResponse()
 
-      return next.handle().pipe(
-         tap(() => {
-            const finish = Date.now()
-            const took = (finish - now).toFixed(3)
-            console.log(
-               `${req.method} ${req.originalUrl} ${res.statusCode} from ${
-                  req.headers['x-forwarded-for'] || req.ip
-               } ${took} ms ${req.headers.origin || ''}`
-            )
-         }),
-         finalize(() => {
-            const finish = Date.now()
-            const took = (finish - now).toFixed(3)
-            console.log(
-               `${req.method} ${req.originalUrl} ${res.statusCode} from ${
-                  req.headers['x-forwarded-for'] || req.ip
-               } ${took} ms ${req.headers.origin || ''}`
-            )
-         })
-      )
+      return next
+         .handle()
+         .pipe(tap(log(req, res, now)), finalize(log(req, res, now)))
    }
 }
